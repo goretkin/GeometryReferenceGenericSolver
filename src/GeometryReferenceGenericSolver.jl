96f461acffa4d3551f4c0ff0ca5313c914b4a757
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2019-11-04 02:49:56 -0500

    Tolerances chosen adhoc, solver fails occasionally still.

--- src/GeometryReferenceGenericSolver.jl
+++ src/GeometryReferenceGenericSolver.jl
@@ -1,3 +1,119 @@
 module GeometryReferenceGenericSolver
 
+using LinearAlgebra: norm
+
+using Parametron
+using Parametron:setdirty!
+using OSQP
+using OSQP.MathOptInterfaceOSQP: OSQPSettings
+
+import MathOptInterface
+const MOI = MathOptInterface
+
+function _constrain_convex_combination_coefficients(model, coeffs)
+    M = length(coeffs)
+    for i = 1:M
+        @constraint(model, coeffs[i] >= 0)
+    end
+    s = sum(coeffs)
+    @constraint(model, s == 1)
+end
+
+function _convex_combination(coeffs, points, m)
+    s = @expression coeffs[1] * points[:, 1]
+    for i = 2:m
+        s = @expression s + coeffs[i] * points[:, i]
+    end
+    return s
+end
+
+function squared_distance(p1, p2, n)
+    s = @expression (p1[1] - p2[1])^2
+    for i = 2:n
+        s = @expression s + (p1[i] - p2[i])^2
+    end
+    return s
+end
+
+struct SolverError{T} <: Exception
+    status::T
+end
+
+function find_minimum_distance(convex1::NTuple{M1, PointT}, convex2::NTuple{M2, PointT}) where {M1, M2, PointT}
+    model = Model(OSQP.Optimizer(
+        max_iter=4000,
+        warm_start=false,   # for deterministic results
+        polish=true,
+        eps_prim_inf=1e-8,
+        eps_dual_inf=1e-8,
+        eps_abs=1e-8,
+        eps_rel=1e-6)
+    )
+
+    # from https://github.com/tkoolen/Parametron.jl/blob/d824c9f9ff4ba7bc38a9407129504bb8ecca31ee/test/model.jl#L22
+    #MOI.set(model.optimizer, OSQPSettings.AdaptiveRhoInterval(), 25) # required for deterministic behavior
+    #MOI.set(model.optimizer, OSQPSettings.Verbose(), false)
+    #MOI.set(model.optimizer, OSQPSettings.Scaling(), false)
+    N = length(PointT)      # TODO works for GeometryType.Point, general trait?
+    # points
+    c1 = Parameter(model, val=zeros(N, M1))
+    c2 = Parameter(model, val=zeros(N, M2))
+
+    # TODO use broadcast machinery to set ]up problem
+    # coefficients of convex combination
+    a1 = [Variable(model) for _ in 1:M1]
+    a2 = [Variable(model) for _ in 1:M2]
+
+    _constrain_convex_combination_coefficients(model, a1)
+    _constrain_convex_combination_coefficients(model, a2)
+
+    p1 = _convex_combination(a1, c1, M1)
+    p2 = _convex_combination(a2, c2, M2)
+    d_sq = squared_distance(p1, p2, N)
+
+    @objective(model, Minimize, d_sq)
+
+    # set parameters
+    for i = 1:M1
+        c1.val[][:,i] = convex1[i]
+    end
+    for i = 1:M2
+        c2.val[][:,i] = convex2[i]
+    end
+
+    setdirty!(c1)
+    setdirty!(c2)
+    #=data_before_solve = deepcopy(
+     unsafe_load_data(
+        unsafe_load(
+            unsafe_load(model.optimizer.inner.workspace).data))
+    )
+    =#
+    data_before_solve = "syke"
+    solve!(model)
+    status =  MOI.get(model.optimizer, MOI.TerminationStatus())
+
+    if status != MOI.OPTIMAL
+        #throw(SolverError(status))
+    end
+
+    # TODO remove after done debugging
+    g = value(model, a1[1])
+
+    # extract decision varibles and re-compute expressions
+    # TODO avoid expressing convex combinations and norm twice
+    v_a1 = value.(model, a1)
+    v_a2 = value.(model, a2)
+
+    v_p1 = sum(v_a1 .* convex1)
+    v_p2 = sum(v_a2 .* convex2)
+    v_d_sq = sum((v_p1 - v_p2).^2)
+    return (d_sq = v_d_sq, p1=v_p1, p2=v_p2, model=model,
+        model_parts = (
+            a1=a1, a2=a2, c1=c1, c2=c2,
+        ),
+        data_before_solve=data_before_solve
+    )
+end
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,49 @@
-using GeometryReferenceGenericSolver
 using Test
+using LinearAlgebra: norm
+using GeometryTypes: Point
+using Random: seed!
+
+using GeometryReferenceGenericSolver
+using GeometryReferenceGenericSolver: find_minimum_distance
+
+r = find_minimum_distance((Point(-1,0), Point(1,0)), (Point(0,-1), Point(0, 1)))
+@test r.d_sq < 100eps()
+@test norm(r.p1 - [0, 0]) < 100eps()
+@test norm(r.p2 - [0, 0]) < 100eps()
+
+xy_plane = 1e8 .* (Point(-1,0,0), Point(1,0,0), Point(0, 1, 0))
+
+seed!(1234)
+@testset "project xy" begin
+    # make all points first, to facilitate test debugging
+    testpoints = [1e5 .* rand(Point{3, Float64}) for i = 1:1000]
+
+    for i = 970:1000
+        @show i
+        p = testpoints[i]
+        @show p
+        # project p onto xy_plane
+        r = find_minimum_distance(xy_plane, (p,))
+        @show r
+        @test isapprox(p[3]^2, r.d_sq)
+        @test norm(r.p1[1:2] - p[1:2]) < 1e-6
+    end
+end
+
+# Somep oints caused an MathOptInterface.ITERATION_LIMIT with
+#=     model = Model(OSQP.Optimizer(
+        max_iter=400000,
+        warm_start=false,   # for deterministic results
+        polish=true,
+        eps_prim_inf=1e-14,
+        eps_dual_inf=1e-14,
+        eps_abs=1e-14,
+        eps_rel=1e-6)
+    )
+    MOI.set(model.optimizer, OSQPSettings.AdaptiveRhoInterval(), 25) #
+=#
+# looks like they're points with low z coordinate.
+
+p = Point(15099.411397006435, 63329.17689456945, 489.2398433903766)
+p = Point(56270.48118496456, 53948.45819773468, 34.889982642960504)
+p = Point(37517.76357686068, 84362.38878880453, 233.50717418757495)
